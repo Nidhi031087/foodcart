6da1c8fe2fbda13f4ffa3e4dccdcced89118434c
Nidhi <65193000+Nidhi031087@users.noreply.github.com>
2021-08-24 17:44:35 +0530

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,41 @@
+const data={
+    products:[{
+        _id:"4",
+        name:"Carrot",
+        image:"/images/d1.jpg",
+        price:"40 per/KG",
+        brand:"Organic"
+    },
+    {
+        _id:"5",
+        name:"Carrot",
+        image:"/images/d1.jpg",
+        price:"40 per/KG",
+        brand:"Organic"
+    },
+    
+    {
+        _id:"1",
+        name:"Carrot",
+        image:"/images/d1.jpg",
+        price:"40 per/KG"
+    },
+
+    {
+        _id:"2",
+        name:"Carrot",
+        image:"/images/d1.jpg",
+        price:"40 per/KG"
+    },
+    {
+        _id:"3",
+        name:"Carrot",
+        image:"/images/d1.jpg",
+        price:"40 per/KG"
+    },
+
+]
+}
+
+
+export default data;
